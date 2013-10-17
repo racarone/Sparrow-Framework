045a4d5af3645c6fe4a3d89e0bf5326fdb3495b0
Robert Carone <racarone@gmail.com>
2013-10-17 14:03:52 -0700

    sync with atomic-pool

--- sparrow/src/Classes/SPPoolObject.m
+++ sparrow/src/Classes/SPPoolObject.m
@@ -73,23 +73,48 @@ SP_INLINE OSQueueHead* getQueueWithClass(Class class)
 
 // --- queue ---------------------------------------------------------------------------------------
 
-#define QUEUE_OFFSET        sizeof(Class)
+#define QUEUE_OFFSET sizeof(Class)
+
+#if SP_POOL_OBJECT_IS_ATOMIC
 #define DEQUEUE(pool)       OSAtomicDequeue(pool, QUEUE_OFFSET)
 #define ENQUEUE(pool, obj)  OSAtomicEnqueue(pool, obj, QUEUE_OFFSET)
+#else
+#define DEQUEUE(pool)       dequeue(pool)
+#define ENQUEUE(pool, obj)  enqueue(pool, obj)
+#endif
+
+void enqueue(OSQueueHead *list, void *new)
+{
+    *((void **)((char *)new + QUEUE_OFFSET)) = list->opaque1;
+    list->opaque1 = new;
+}
+
+void* dequeue(OSQueueHead *list)
+{
+    void *head;
+
+    head = list->opaque1;
+    if (head != NULL) {
+        void **next = (void **)((char *)head + QUEUE_OFFSET);
+        list->opaque1 = *next;
+    }
+
+    return head;
+}
 
 // --- class implementation ------------------------------------------------------------------------
 
-#if SP_POOL_OBJECT_IS_ATOMIC
-    #define INCREMENT_32(var)    OSAtomicIncrement32Barrier(&var)
-    #define DECREMENT_32(var)    OSAtomicDecrement32Barrier(&var)
-    #define MEMORY_BARRIER()     OSMemoryBarrier()
-#else
-    #define INCREMENT_32(var)    (++ var)
-    #define DECREMENT_32(var)    (-- var)
-    #define MEMORY_BARRIER()
-#endif
-
 #define RETAIN_COUNT _refOrLink.ref
+
+#if SP_POOL_OBJECT_IS_ATOMIC
+#define INCREMENT_32(var)    OSAtomicIncrement32Barrier(&var)
+#define DECREMENT_32(var)    OSAtomicDecrement32Barrier(&var)
+#define MEMORY_BARRIER()     OSMemoryBarrier()
+#else
+#define INCREMENT_32(var)    (++ var)
+#define DECREMENT_32(var)    (-- var)
+#define MEMORY_BARRIER()
+#endif
 
 @implementation SPPoolObject
 {
@@ -111,12 +136,12 @@ SP_INLINE OSQueueHead* getQueueWithClass(Class class)
 
 + (id)allocWithZone:(NSZone *)zone
 {
-  #if DEBUG && !SP_POOL_OBJECT_IS_ATOMIC
+#if DEBUG && !SP_POOL_OBJECT_IS_ATOMIC
     // make sure that people don't use pooling from multiple threads
     static id thread = nil;
     if (thread) NSAssert(thread == [NSThread currentThread], @"SPPoolObject is NOT thread safe! Must set SP_POOL_OBJECT_IS_ATOMIC to 1.");
     else thread = [NSThread currentThread];
-  #endif
+#endif
 
     OSQueueHead *poolQueue = getQueueWithClass(self);
     SPPoolObject *object = DEQUEUE(poolQueue);
